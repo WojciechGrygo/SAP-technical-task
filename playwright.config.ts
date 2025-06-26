a46241eb43213f0539f79d50964162af1a1b5356
Wojciech Grygo <ce-wojciech.grygo@regnology.net>
2025-06-26 10:30:04 +0200

    Add Playwright configuration file for test setup

---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30_000,
+  expect: { timeout: 5_000 },
+  fullyParallel: true,
+  retries: process.env.CI ? 1 : 0,
+  workers: 3,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://www.sapfioneer.com',
+    actionTimeout: 5_000,
+    trace: 'retain-on-failure',
+    video: 'off',
+    screenshot: 'only-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
