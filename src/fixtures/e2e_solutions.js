6f3a81d06cb4ed5fabd4c325bb6afc1c49b0fd42
Wojciech Grygo <ce-wojciech.grygo@regnology.net>
2025-06-26 10:30:28 +0200

    Add e2e_solutions.js and pageData.json

---
+++ src/fixtures/e2e_solutions.js
@@ -0,0 +1,19 @@
+const e2e_solutions_for_financial_services = [
+    {
+      header: "Banking",
+      body: "Reach more customers and build trust with real-time financial insights and risk control. Join the 800 banks globally who have chosen our platform.",
+      link: "https://www.sapfioneer.com/banking/"
+    },
+    {
+      header: "Insurance",
+      body: "Accelerate digital insurance processes to meet customer expectations and create new revenue streams by connecting easily to the ecosystem.",
+      link: "https://www.sapfioneer.com/insurance/"
+    },
+    {
+      header: "Finance & ESG",
+      body: "Establish a single source of truth for profitability and turn compliance into your advantage using our integrated finance & ESG platform.",
+      link: "https://www.sapfioneer.com/finance-esg/"
+    }
+]
+
+export default e2e_solutions_for_financial_services;
